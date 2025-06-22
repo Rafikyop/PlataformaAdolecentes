c1558365c7d7dd6dbcd0f2b3fe687a1f6069a6d0
Rafiky <rafikyop@gmail.com>
2025-06-22 18:50:52 -0500

    Se comentaron las lineas awt para no tenes advertencias y tener el codigo mas limpio

--- src/main/java/com/plataforma/proyecto/AppGUI.java
+++ src/main/java/com/plataforma/proyecto/AppGUI.java
@@ -7,8 +7,8 @@ import com.plataforma.proyecto.dto.IntervencionDTO;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 import java.util.List;
 
 public class AppGUI extends JFrame {
